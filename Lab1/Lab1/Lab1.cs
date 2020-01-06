2c41f5056487d86820e9e55f292116d92f20fd83
EuiJeong <wftw0121@naver.com>
2020-01-06 16:28:19 +0900

    Lab1 commit

---
+++ Lab1/Lab1/Lab1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1
+{
+    class Lab1
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
