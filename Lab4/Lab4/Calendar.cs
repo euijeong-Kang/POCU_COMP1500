ebf918328e54e780ca4b0e1bafd7927a233dfd97
EuiJeong <wftw0121@naver.com>
2020-01-27 13:32:12 +0900

    Lab4 Commint

---
+++ Lab4/Lab4/Calendar.cs
@@ -0,0 +1,37 @@
+﻿namespace Lab4
+{
+    public static class Calendar
+    {
+        public static bool IsLeapYear(uint year)
+        {
+            bool bResult = false;
+
+            if (year % 4 == 0 && year % 100 != 0)
+            {
+                bResult = true;
+            }
+            else if (year % 100 == 0 && year % 400 == 0)
+            {
+                bResult = true;
+            }
+            
+            return bResult;
+        }
+
+        public static int GetDaysInMonth(uint year, uint month)
+        {
+            int result = 0;
+            int[] monthPerDay = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            if(IsLeapYear(year) == false)
+            {
+                result = monthPerDay[month - 1];
+            }
+            else if (IsLeapYear(year) == true)
+            {
+                monthPerDay[1] += 1;
+                result = monthPerDay[month - 1];
+            }
+            return result;
+        }
+    }
+}
--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Lab4
 {
@@ -6,7 +7,7 @@ namespace Lab4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
