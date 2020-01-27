34c3cf0aabb7493675843639629f0df141489126
EuiJeong <wftw0121@naver.com>
2020-01-27 22:07:08 +0900

    commit

--- Assignment1/Assignment1/Assignment1.cs
+++ Assignment1/Assignment1/Assignment1.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System;
 
 namespace Assignment1
 {
@@ -6,11 +7,96 @@ namespace Assignment1
     {
         public static void PrintIntegers(StreamReader input, StreamWriter output, int width)
         {
+            if (width < 10)
+            {
+                width = 10;
+            }
+            string intString = "int";
+            string hexString = "hex";
+            string otcString = "oct";
+            string boder = "";
 
+            string read1 = input.ReadLine();
+            string read2 = input.ReadLine();
+            string read3 = input.ReadLine();
+            string read4 = input.ReadLine();
+            string read5 = input.ReadLine();
+
+            intString = intString.PadLeft(width, ' ');
+            hexString = hexString.PadLeft(width, ' ');
+            otcString = otcString.PadLeft(width, ' ');
+
+            output.WriteLine($"{otcString} {intString} {hexString}");
+
+            output.WriteLine($"{boder.PadLeft(width, '-')} {boder.PadLeft(width, '-')} {boder.PadLeft(width, '-')}");
+
+            intString = read1.PadLeft(width, ' ');
+            hexString = Convert.ToString(int.Parse(read1), 16).PadLeft(width, ' ');
+            otcString = Convert.ToString(int.Parse(read1), 8).PadLeft(width, ' ');
+
+            output.WriteLine($"{otcString} {intString} {hexString}");
+            //
+            intString = read2.PadLeft(width, ' ');
+            hexString = Convert.ToString(int.Parse(read2), 16).PadLeft(width, ' ');
+            otcString = Convert.ToString(int.Parse(read2), 8).PadLeft(width, ' ');
+
+            output.WriteLine($"{otcString} {intString} {hexString}");
+            //
+            intString = read3.PadLeft(width, ' ');
+            hexString = Convert.ToString(int.Parse(read3), 16).PadLeft(width, ' ');
+            otcString = Convert.ToString(int.Parse(read3), 8).PadLeft(width, ' ');
+
+            output.WriteLine($"{otcString} {intString} {hexString}");
+            //
+            intString = read4.PadLeft(width, ' ');
+            hexString = Convert.ToString(int.Parse(read4), 16).PadLeft(width, ' ');
+            otcString = Convert.ToString(int.Parse(read4), 8).PadLeft(width, ' ');
+
+            output.WriteLine($"{otcString} {intString} {hexString}");
+            //
+            intString = read5.PadLeft(width, ' ');
+            hexString = Convert.ToString(int.Parse(read5), 16).PadLeft(width, ' ');
+            otcString = Convert.ToString(int.Parse(read5), 8).PadLeft(width, ' ');
+
+            output.WriteLine($"{otcString} {intString} {hexString}");
         }
 
         public static void PrintStats(StreamReader input, StreamWriter output)
         {
+            string num1 = input.ReadLine();
+            string num2 = input.ReadLine();
+            string num3 = input.ReadLine();
+            string num4 = input.ReadLine();
+            string num5 = input.ReadLine();
+
+            double[] doubleArray = new double[5] { double.Parse(num1), double.Parse(num2), double.Parse(num3), double.Parse(num4), double.Parse(num5) };
+            Array.Sort(doubleArray);
+            string sum = (double.Parse(num1) + double.Parse(num2) + double.Parse(num3) + double.Parse(num4) + double.Parse(num5)).ToString();
+            sum = string.Format("{0:F3}", double.Parse(sum));
+            string average = (double.Parse(sum) / 5).ToString();
+            average = string.Format("{0:F3}", double.Parse(average));
+
+            string min = string.Format("{0:F3}", doubleArray[0]);
+            string max = string.Format("{0:F3}", doubleArray[4]);
+
+            num1 = string.Format("{0:F3}", double.Parse(num1));
+            num2 = string.Format("{0:F3}", double.Parse(num2));
+            num3 = string.Format("{0:F3}", double.Parse(num3));
+            num4 = string.Format("{0:F3}", double.Parse(num4));
+            num5 = string.Format("{0:F3}", double.Parse(num5));
+
+            output.WriteLine(num1.PadLeft(25, ' '));
+            output.WriteLine(num2.PadLeft(25, ' '));
+            output.WriteLine(num3.PadLeft(25, ' '));
+            output.WriteLine(num4.PadLeft(25, ' '));
+            output.WriteLine(num5.PadLeft(25, ' '));
+            output.WriteLine($"Min{min.PadLeft(22, ' ')}");
+            output.WriteLine($"Max{max.PadLeft(22, ' ')}");
+            output.WriteLine($"Sum{sum.ToString().PadLeft(22, ' ')}");
+            output.WriteLine($"Average{average.ToString().PadLeft(18, ' ')}");
+
+
+
 
         }
     }
--- Assignment1/Assignment1/Program.cs
+++ Assignment1/Assignment1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.IO;
+using System;
 
 namespace Assignment1
 {
@@ -6,7 +7,7 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
